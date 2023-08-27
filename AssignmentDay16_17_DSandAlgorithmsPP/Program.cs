cc7f5fe6301102ff8767c85a98ee5ccf14bc3aee
khushbu <khushbusoni475@gmail.com>
2023-08-27 12:04:35 +0530

    [khushbu] Adding Hello massege

---
+++ AssignmentDay16_17_DSandAlgorithmsPP/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentDay16_17_DSandAlgorithmsPP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Word");
+            Console.ReadLine();
+        }
+    }
+}
